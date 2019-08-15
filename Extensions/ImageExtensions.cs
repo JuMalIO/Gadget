1dc8a10d5793e70f494f1467bb53735ec03d13a7
JuMalIO <spamjulius@mail.com>
2019-08-15 17:42:46 +0300

    Remove unused settings; change app.manifest

---
+++ Extensions/ImageExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace Gadget.Extensions
+{
+    public static class ImageExtensions
+    {
+        public static Image Resize(this Image image, int x, int y)
+        {
+            if (image != null)
+            {
+                if (image.Width > x || image.Height > y)
+                {
+                    if (image.Width > image.Height)
+                    {
+                        y = (int)((double)x * (double)((double)image.Height / (double)image.Width));
+                    }
+                    else
+                    {
+                        x = (int)((double)y * (double)((double)image.Width / (double)image.Height));
+                    }
+                    return (Image)(new Bitmap(image, new Size(x, y)));
+                }
+
+                return image;
+            }
+
+            return null;
+        }
+    }
+}
