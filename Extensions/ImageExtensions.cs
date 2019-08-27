58c5c5298c35d07b13f4d4ee637f747fa2cdcc77
JuMalIO <spamjulius@mail.com>
2019-08-27 23:11:12 +0300

    Image extensions

--- Extensions/ImageExtensions.cs
+++ Extensions/ImageExtensions.cs
@@ -4,25 +4,48 @@ namespace Gadget.Extensions
 {
     public static class ImageExtensions
     {
+        private static readonly float _scale;
+
+        static ImageExtensions()
+        {
+            using (Bitmap b = new Bitmap(1, 1))
+            {
+                _scale = b.HorizontalResolution / 96.0f;
+            }
+        }
+
+        public static int GetWidth(this Image image)
+        {
+            return (int)(image.Width * _scale);
+        }
+
+        public static int GetHeight(this Image image)
+        {
+            return (int)(image.Height * _scale);
+        }
+
         public static Image Resize(this Image image, int x, int y)
         {
             if (image == null)
-                return null;
-
-            if (image.Width > x || image.Height > y)
             {
-                if (image.Width > image.Height)
-                {
-                    y = (int)((double)x * (double)((double)image.Height / (double)image.Width));
-                }
-                else
-                {
-                    x = (int)((double)y * (double)((double)image.Width / (double)image.Height));
-                }
-                return (Image)(new Bitmap(image, new Size(x, y)));
+                return null;
             }
 
-            return image;
+            if (image.Width < x && image.Height < y)
+            {
+                return image;
+            }
+            
+            if (image.Width > image.Height)
+            {
+                y = (int)(x * (image.Height / (double)image.Width));
+            }
+            else
+            {
+                x = (int)(y * (image.Width / (double)image.Height));
+            }
+
+            return new Bitmap(image, new Size(x, y));
         }
     }
 }
